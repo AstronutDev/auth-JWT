e06a094e2b48c68c0aa862da2b4c21f5d8d47516
Supanut Laddayam <ynotmela@gmail.com>
2020-10-21 19:59:56 +0700

    add logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -89,3 +89,8 @@ module.exports.login_post = async (req, res, next) => {
         res.status(400).json({ errors })
     }
 }
+
+module.exports.logout_get = (req, res, next) => {
+    res.cookie('jwt', '', { maxAge: 1} )
+    res.redirect('/')
+}
